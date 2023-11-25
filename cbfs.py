d48c2af7c0f21bd34adfd05d0090d98aca456618
paolo.oppelt@gmail.com <paolo.oppelt@gmail.com>
2023-11-25 12:51:47 +0100

    added some comments

--- cbfs.py
+++ cbfs.py
@@ -31,8 +31,11 @@ elastic_vector_search = ElasticsearchStore(
     es_password=_es_password,
 )
 
+# PROMPT TEMPLATE
+# No_Doc is a string to be used when no relevant documents are found
 No_Doc = "Die hinterlegten Leitlinien Dokumente enthalten keine Informationen zu Ihrer Frage."
 
+# defines a prompt template in a structured format, guiding how the AI should format its responses
 template = """Beantworten Sie die Frage am Ende des Textes anhand der folgenden Informationen. 
 Geben Sie bei der Antwort so viele relevante Hintergrundinformationen mit wie Möglich.
 Die Antwort sollte nicht länger als 8 Sätze sein.
@@ -42,10 +45,17 @@ Frage: {question}
 Hilfreiche Antwort:"""
 prompt = PromptTemplate.from_template(template)
 
+# MODEL LOADING FUNCTION
+# This function initializes a conversational retrieval chain model
+
 def load_model():
     qa = ConversationalRetrievalChain.from_llm(
+        # ChatOpenAI as the language model
         llm=ChatOpenAI(temperature=0, model="gpt-4-1106-preview"), # gpt-3.5-turbo
+        # vectordb as a retriever
         retriever=elastic_vector_search.as_retriever(search_kwargs={"k": 3}),
+        # It specifies parameters like the prompt template, the response if no documents are found,
+        # and returns both the answer and source documents.
         combine_docs_chain_kwargs={"prompt": prompt},
         response_if_no_docs_found = No_Doc,
         return_source_documents=True,
@@ -54,17 +64,22 @@ def load_model():
     return qa 
 
 
+# CLASS DEFINITION
+# cbfs is a class with a chat_history 
 class cbfs(param.Parameterized):
     chat_history = param.List([])
 
+# __init__ initializes the model using 'load_model'
     def __init__(self,  **params):
         super(cbfs, self).__init__( **params)
         self.qa = load_model()
     
+# convchain is a method to process a query and update the chat history with the query and its reponse
     def convchain(self, query):
         result = self.qa({"question": query, "chat_history": self.chat_history})
         self.chat_history.extend([(query, result["answer"])])
         return result
 
+# is a method to clear the chat history
     def clr_history(self):
         self.chat_history = []
